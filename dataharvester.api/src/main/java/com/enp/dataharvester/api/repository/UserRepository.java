26d1a76cbb3ede2c1080e080a0ad3091747973c4
eneperezv <eneperezv@gmail.com>
2024-11-24 22:43:28 -0500

    Creacion e implementacion de clases para seguridad JWT. Revision de application.properties. Revision de dependencias
     Changes to be committed:
    	modified:   dataharvester.api/pom.xml
    	modified:   dataharvester.api/src/main/java/com/enp/dataharvester/api/entity/ErrorDetails.java
    	modified:   dataharvester.api/src/main/java/com/enp/dataharvester/api/entity/ResponseDetails.java
    	modified:   dataharvester.api/src/main/java/com/enp/dataharvester/api/entity/Token.java
    	modified:   dataharvester.api/src/main/java/com/enp/dataharvester/api/entity/User.java
    	modified:   dataharvester.api/src/main/java/com/enp/dataharvester/api/entity/UserMyDetails.java
    	modified:   dataharvester.api/src/main/java/com/enp/dataharvester/api/repository/UserRepository.java
    	new file:   dataharvester.api/src/main/java/com/enp/dataharvester/api/security/JwtAuthenticationFilter.java
    	new file:   dataharvester.api/src/main/java/com/enp/dataharvester/api/security/JwtService.java
    	new file:   dataharvester.api/src/main/java/com/enp/dataharvester/api/security/LoginForm.java
    	new file:   dataharvester.api/src/main/java/com/enp/dataharvester/api/security/WebSecurityConfig.java
    	modified:   dataharvester.api/src/main/resources/application.properties

--- dataharvester.api/src/main/java/com/enp/dataharvester/api/repository/UserRepository.java
+++ dataharvester.api/src/main/java/com/enp/dataharvester/api/repository/UserRepository.java
@@ -1,5 +1,15 @@
 package com.enp.dataharvester.api.repository;
 
-public interface UserRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.enp.dataharvester.api.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+	
+	Optional<User> findByUsername(String username);
 
 }
---
+++ dataharvester.api/src/main/java/com/enp/dataharvester/api/security/LoginForm.java
@@ -0,0 +1,5 @@
+package com.enp.dataharvester.api.security;
+
+public record LoginForm(String username, String password) {
+
+}
